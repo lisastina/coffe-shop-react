63b57e18f865a13b9b2ecfd3163d64a3fa553c08
Lisa Hansson <lisastina.hansson@gmail.com>
2022-03-31 13:55:41 +0200

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,38 @@
+import OpeningHours from "./OpeningHours";
+
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="content">
+      <div className="hero">
+        {/*  Beans */}
+        {/* <div class="bean wiggle hero__bean-1">
+        <img id="bean-1" src="{{ bean_1_path }}" alt="coffe bean">
+      </div>
+      <div class="bean wiggle hero__bean-2">
+        <img id="bean-2" src="{{ bean_2_path }}" alt="coffe bean">
+      </div>
+      <div class="bean wiggle hero__bean-3">
+        <img id="bean-2" src="{{ bean_2_path }}" alt="coffe bean">
+      </div>
+      <div class="bean wiggle hero__bean-4">
+        <img id="bean-3" src="{{ bean_3_path }}" alt="coffe bean">
+      </div>
+      <div class="bean wiggle hero__bean-5">
+        <img id="bean-4" src="{{ bean_4_path }}" alt="coffe bean">
+      </div> */}
+
+        <h2 className="hero__subtitle">Welcome to</h2>
+
+        <h1 className="hero__title">Lisa's Coffee Shop</h1>
+
+        <div className="hero__image">
+          <img src="" alt="coffe-cheers" />
+        </div>
+
+        <h2 className="hero__subtitle">We serve coffee!</h2>
+      </div>
+
+      <OpeningHours />
     </div>
   );
 };
