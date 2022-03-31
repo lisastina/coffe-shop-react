a9c6ec71fdd18f40ef8cd1c82093f0a4047707de
Lisa Hansson <lisastina.hansson@gmail.com>
2022-03-31 14:03:19 +0200

    opening hours

--- src/components/OpeningHours.jsx
+++ src/components/OpeningHours.jsx
@@ -1,7 +1,12 @@
 const OpeningHours = () => {
   return (
-    <div>
-      <h1>opening hours</h1>
+    <div class="opening-hours">
+      <ul>
+        <h2>Opening hours</h2>
+        <li>Monday - Thursday: 7:00 - 19:00</li>
+        <li>Saturday: 10:00 - 20:00</li>
+        <li>Sunday: 11:00 - 18:00</li>
+      </ul>
     </div>
   );
 };
